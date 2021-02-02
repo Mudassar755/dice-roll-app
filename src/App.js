5f9cabaf93f934bf298a2157bcc91f72de060a05
mudassar755 <mudasar.se@gmail.com>
2021-02-02 11:34:12 +0500

    initial comit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import DiceROll from './components/DiceRoll'
+
+function App() {
+  return (
+    <div className="App">
+     <DiceROll />
+    </div>
+  );
+}
+
+export default App;
